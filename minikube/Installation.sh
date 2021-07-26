14c9ac3252c35dd221eba4f6a5180a51a2bf42cb
retheshnair <retheshnair@users.noreply.github.com>
2021-07-26 18:14:47 +0530

    Update Installation.sh

--- minikube/Installation.sh
+++ minikube/Installation.sh
@@ -5,4 +5,5 @@ https://v1-18.docs.kubernetes.io/docs/tasks/tools/install-minikube/
 
 
 # start multiple node minikube 
-minikube start --nodes 3 -p minikube
+minikube start --nodes 3 -p multinode-demo
+minikube profile multinode-demo
