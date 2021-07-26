78ce62266c6db71de6211a8ee1466b7a85380e58
retheshnair <retheshnair@users.noreply.github.com>
2021-07-26 18:21:18 +0530

    Update Installation.sh

--- minikube/Installation.sh
+++ minikube/Installation.sh
@@ -1,4 +1,8 @@
 
+wget https://github.com/sharkdp/bat/releases/download/v0.15.4/bat_0.15.4_amd64.deb
+
+sudo dpkg -i ./bat_0.15.4_amd64.deb
+
 #Check the OS details before installation
 
 https://v1-18.docs.kubernetes.io/docs/tasks/tools/install-minikube/
