e82a9c05858ba4c0373792c1e576dfc779f4f87f
retheshnair <retheshnair@users.noreply.github.com>
2021-07-26 18:07:09 +0530

    Update and rename Installation to Installation.sh

---
+++ minikube/Installation.sh
@@ -0,0 +1,8 @@
+
+#Check the OS details before installation
+
+https://v1-18.docs.kubernetes.io/docs/tasks/tools/install-minikube/
+
+
+# start multiple node minikube 
+minikube start --nodes 3 -p minikube
