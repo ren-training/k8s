38db038fdbe7a6c87afb3178086672073dd24d86
retheshnair <retheshnair@users.noreply.github.com>
2021-07-26 18:28:22 +0530

    Update Installation.sh

--- minikube/Installation.sh
+++ minikube/Installation.sh
@@ -1,4 +1,6 @@
 
+#Installation of Bat tool on Linux
+
 wget https://github.com/sharkdp/bat/releases/download/v0.15.4/bat_0.15.4_amd64.deb
 
 sudo dpkg -i ./bat_0.15.4_amd64.deb
@@ -9,5 +11,13 @@ https://v1-18.docs.kubernetes.io/docs/tasks/tools/install-minikube/
 
 
 # start multiple node minikube 
-minikube start --nodes 3 -p multinode-demo
+minikube start --nodes 4 -p multinode-demo
+
+#Select the profile 
+minikube profile list
 minikube profile multinode-demo
+
+
+#Setup of the Ingress Controller for Minikube 
+
+minikube addons enable ingress
