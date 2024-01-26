949e609b72b51e3eaf268b66cdf4219ab804eba1
retheshnair <retheshnair@users.noreply.github.com>
2024-01-26 17:57:47 +0400

    Update helm.sh

--- helm/helm.sh
+++ helm/helm.sh
@@ -5,3 +5,10 @@ helm repo <COMMAND>
 helm repo list
 helm repo add REPO-NAME REPO-URL
 helm search repo <COMMAND>
+helm repo list
+helm repo add
+helm repo update
+helm search repo
+helm install
+helm list
+helm unins
